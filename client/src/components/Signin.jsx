afb16219931c2d4c1ad120b85007a37cf9c33963
karthick <93571352+Karthick1242004@users.noreply.github.com>
2024-03-15 09:25:26 +0530

    Update Signin.jsx

--- client/src/components/Signin.jsx
+++ client/src/components/Signin.jsx
@@ -13,7 +13,7 @@ const Signin= () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const response = await axios.post('http://localhost:8000/login', { username, password });
+      const response = await axios.post('https://kec-vms.onrender.com/login', { username, password });
       
       if(response.status === 200){ 
         toast.success('Logined successfully!');
